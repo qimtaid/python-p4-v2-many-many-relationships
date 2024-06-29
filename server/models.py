f0b2be396b161bde72f4011e8c47fb9a2685cb63
Dan Koskei <qimtaid@gmail.com>
2024-06-29 16:13:25 +0300

    Complete lab

--- server/models.py
+++ server/models.py
@@ -10,6 +10,15 @@ metadata = MetaData(naming_convention={
 
 db = SQLAlchemy(metadata=metadata)
 
+#Association table to store many-to-many relationship between employees and meetings
+employee_meetings = db.Table(
+    'employee_meetings',
+    metadata,
+    db.Column('employee_id', db.Integer, db.ForeignKey(
+        'employees.id'), primary_key=True),
+    db.COlumn('meeting_id', db.Integer, db.ForeignKey(
+        'meetings.id'), primary_key=True)
+    )
 
 class Employee(db.Model):
     __tablename__ = 'employees'
@@ -17,6 +26,18 @@ class Employee(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String)
     hire_date = db.Column(db.Date)
+    
+    #Relationship mapping the employee to related meetings
+    meetings = db.relationship(
+        'Meeting', secondary=employee_meetings, back_populates='employees')
+    
+    #Relationship mapping the employee to related assignments
+    assignments = db.relationship(
+        'Assignment', back_populates='employee', cascade='all, delete-orphan')
+    
+    #Association proxy to get projects for this employee through assigments
+    projects = association_proxy('assignments', 'project',
+                                 creator=lambda project_obj: Assignment(project=project_obj))
 
     def __repr__(self):
         return f'<Employee {self.id}, {self.name}, {self.hire_date}>'
@@ -40,6 +61,34 @@ class Project(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String)
     budget = db.Column(db.Integer)
+    
+    #Relationship mapping the project to related assignments
+    assignments = db.relationship('Assignment', back_populates='project', cascade='all, delete-orphan')
+    
+    #Association proxy to get employees for this project through assignments
+    employees = association_proxy('assignments', 'employee',
+                                  creator=lambda employee_obj: Assignment(employee=employee_obj))
+    
+class Assignment(db.Model): 
+    _tablename_ = 'assignments'
+    
+    id = db.Column(db.Integer, primary_key=True)
+    role = db.Column(db.String)
+    start_date = db.Column(db.DateTime)
+    end_date = db.Column(db.DateTime)
+    
+#Foreign key to store the employee id
+employee_id = db.Column(db.Integer, db.ForeignKey('employee.id'))
+
+#Foreign key to store the project id
+project_id = db.Column(db.Integer, db.ForeignKey('project.id'))
+
+
+#Relationship mapping the assignment to related employee
+employee = db.relationship('Employee', back_populates='assignments')
+#Relationship mapping the assignment to related project
+project = db.relationship('Project', back_populates='assignments')
+
 
     def __repr__(self):
         return f'<Review {self.id}, {self.title}, {self.budget}>'
